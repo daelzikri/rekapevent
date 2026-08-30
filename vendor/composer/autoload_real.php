0eeb70c76a6b4403cc0c6cd9947d62f553efb9d0
daelzikri <daelzik51@gmail.com>
2026-08-30 20:49:37 +0800

    fitur backup otomatis

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInita81b59a06bd52d82b27f78b318d1ef56
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInita81b59a06bd52d82b27f78b318d1ef56::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
